f2105dde2e7db38da28f04d8bec935f1e33bdb9a
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-30 15:18:00 +0900

    feat: 메인 페이지에 이미지 추가

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-8">
       <h1 className="text-3xl font-bold my-6">Home</h1>
+      <Image
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4Xqgo96oN5pNB0wkwxg0i9pz4yJp9EgszKWeZN-9B9A&s"
+        width={400}
+        height={400}
+        alt=""
+      />
+      {/* 중요한 정보를 담지 않고 있는 이미지라서 alt="" 처리*/}
       <p className="mb-12 font-medium text-gray-700">
         할 일을 관리해 생산성을 높이는 Todo App
       </p>
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
