34165696b6d67fdab665015672fcb379607b4989
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-30 11:08:23 +0900

    feat: todos 페이지 생성 및 홈에서 라우팅 연결

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="p-8">
+    <main className="flex flex-col items-center p-8">
       <h1 className="text-3xl font-bold my-6">Home</h1>
-      <p className="mb-6 font-medium text-gray-700">
+      <p className="mb-12 font-medium text-gray-700">
         할 일을 관리해 생산성을 높이는 Todo App
       </p>
+      <Link
+        href="/todos"
+        className="inline-block py-3 px-6 bg-black text-white rounded-lg "
+      >
+        Todo 목록 보기
+      </Link>
     </main>
   );
 }
---
+++ app/todos/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <h1>Todo List</h1>
+    </main>
+  );
+};
+
+export default page;
