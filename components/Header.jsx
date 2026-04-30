7220bdba4cb922774f5833b9514868bf668bd778
yooseohyeon <yooseohyeon.02@gmail.com>
2026-04-30 15:37:40 +0900

    feat: Header 컴포넌트에 todo 목록, todo 추가 라우팅 연결

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-10 w-full p-6 border-b">
+    <header className="sticky top-0 z-10 flex items-center gap-10 w-full p-6 px-14 border-b bg-white">
       <Link href="/">
         <h1 className="text-xl font-semibold">Todo App</h1>
       </Link>
+
+      <div className="flex gap-6">
+        <Link href="/todos">Todo 목록</Link>
+        <Link href="/todos/new">Todo 추가</Link>
+      </div>
     </header>
   );
 };
